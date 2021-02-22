d056fbe370e282a019c2c32fe70c29b00ff0ddc5
Pouya Moradian <pouya.moradian@hotmail.com>
2021-02-22 16:24:16 +0330

    message is sent and received to and from server.

--- src/Grpc.Sidecar.Client/Internal/Middlewares/GrpcRequestHandlerMiddleware.cs
+++ src/Grpc.Sidecar.Client/Internal/Middlewares/GrpcRequestHandlerMiddleware.cs
@@ -1,9 +1,15 @@
-﻿using Grpc.Sidecar.Client.Internal.ContractResolver;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Sidecar.Client.Internal.ContractResolver;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using ProtoBuf;
+using ProtoBuf.Grpc.Client;
+using ProtoBuf.Grpc.Configuration;
 using System;
 using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -43,13 +49,52 @@ namespace Grpc.Sidecar.Client.Internal.Middlewares
                 var parameter = methodInfo.GetParameters();
                 var resolvedType = parameter[0].ParameterType;
 
-                //Deserializeing message
-                if (resolvedType is not null)
-                {
-                    Stream stream = new MemoryStream(data.AsSpan(5).ToArray());
-                    var greetingRequest = Serializer.Deserialize(resolvedType, stream);
-                }
+                ////Deserializeing message
+                //if (resolvedType is not null)
+                //{
+                Stream stream = new MemoryStream(data.AsSpan(5).ToArray());
+                var greetingRequest = Serializer.Deserialize(resolvedType, stream);
+                //}
 
+
+                var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+
+                var createGrpcServiceMethodInfo = typeof(GrpcClientFactory).GetMethod(nameof(GrpcClientFactory.CreateGrpcService), new Type[] { typeof(ChannelBase), typeof(ClientFactory) })
+                    .MakeGenericMethod(methodInfo.DeclaringType);
+
+                var mehtodArg = Expression.Parameter(typeof(GrpcChannel));
+                var mehtodArg2 = Expression.Parameter(typeof(ClientFactory));
+                var body = Expression.Call(createGrpcServiceMethodInfo, mehtodArg, mehtodArg2);
+
+                var expr = Expression.Lambda<Func<GrpcChannel, ClientFactory, object>>(body, mehtodArg, mehtodArg2);
+
+                var function = expr.Compile();
+
+                var service = function(channel, null);
+
+                var method = service.GetType().GetRuntimeMethods().FirstOrDefault(t => t.Name.Contains(methodInfo.Name));
+
+                dynamic result = method.Invoke(service, new object[] { greetingRequest, null });
+                await result;
+                var response = result.GetAwaiter().GetResult();
+
+                var converted = Convert.ChangeType(response, methodInfo.ReturnType.GetGenericArguments()[0]);
+
+                using var responseMemoryStream = new MemoryStream();
+                using var responseDataMemoryStream = new MemoryStream();
+                
+                Serializer.Serialize(responseDataMemoryStream, response);
+                
+                await responseMemoryStream.WriteAsync(new byte[] {0,0,0,0, (byte)responseDataMemoryStream.Length});
+                
+                context.Response.Headers.Add("content-type",new Microsoft.Extensions.Primitives.StringValues("application/grpc"));
+                
+                await responseMemoryStream.WriteAsync(responseDataMemoryStream.ToArray());
+
+                await responseMemoryStream.CopyToAsync(context.Response.Body);
+                
+                
                 //Resolving service description
 
                 //Descovering the service target
